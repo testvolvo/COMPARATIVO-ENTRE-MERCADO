98e3ec4f83d25c728c6410e52d28860c5f43d15b
testvolvo <t3231216@gmail.com>
2026-07-27 11:08:29 -0400

    Delete actualizador directory

--- actualizador/Configurar_Actualizador.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-set "SCRIPT=%~dp0actualizar_comparativo.ps1"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%" -Configurar
-set "CODE=%ERRORLEVEL%"
-echo.
-pause
-exit /b %CODE%
